d490e107d971e028164d0af509531e8f52a3b628
izan232 <izanmurcia232@gmail.com>
2025-11-06 15:29:13 +0100

    a

---
+++ src/estado.scala
@@ -0,0 +1,14 @@
+import scala.util.Random
+//creamos un enum para jugador
+enum jugador:
+  case liebre, sabuesos
+//creamos la funcion que sorteara el turno del jugador al comienzo de la partida
+def sortearturno(): jugador =
+  if scala.util.Random.nextBoolean() then jugador.liebre
+  else jugador.sabuesos
+//case class estado con toda la informacion necesaria del estado de la partida
+case class estado(
+  liebre: posicion,
+  sabuesos: set[Posicion],
+                 
+                 )
